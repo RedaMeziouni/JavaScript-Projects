f1e89bfc761a4d9995a1ee14a7ac08dd8a9302d0
RedaMeziouni <meziounidb@gmail.com>
2022-08-07 12:53:45 +0100

    Create initializer function to make all the options behaves on the default settings

---
+++ Rich_Text_Editor/app.js
@@ -0,0 +1,51 @@
+// Get access to all the necessary Elements 
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+
+// Google font list stored inside an array[]
+let fontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courier New",
+    "cursive",
+  ];
+
+// All setting (optins) initializer
+const initializer = () => {
+    //function calls for highlighting buttons
+    //No highlights for link, unlink,lists, undo,redo since they are one time operations
+    highlighter(alignButtons, true);
+    highlighter(spacingButtons, true);
+    highlighter(formatButtons, false);
+    highlighter(scriptButtons, true);
+  
+    //create options for font names using maps
+    fontList.map((value) => {
+      let option = document.createElement("option");
+      option.value = value;
+      option.innerHTML = value;
+      fontName.appendChild(option);
+    });
+  
+    //fontSize allows only till 7
+    for (let i = 1; i <= 7; i++) {
+      let option = document.createElement("option");
+      option.value = i;
+      option.innerHTML = i;
+      fontSizeRef.appendChild(option);
+    }
+  
+    //default size
+    fontSizeRef.value = 3;
+  };
