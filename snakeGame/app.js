5ef752f886fd95dbf416c0c3006827acf4480fc0
RedaMeziouni <meziounidb@gmail.com>
2022-10-14 13:07:23 +0100

    create the check, display and game over functions, also formatted the code

--- snakeGame/app.js
+++ snakeGame/app.js
@@ -31,51 +31,69 @@ let snake = [
 // Event Listner to the arrow keys
 window.addEventListener("keydown", changeDirection);
 
-// Event Listner to the reset button to restart the Game
+//Event listner on the reset game button
 resetBtn.addEventListener("click", resetGame);
 
+// Invoke the start game function
 gameStart();
-// createFood();
-// drawFood();
 
 function gameStart() {
   running = true;
+
   scoreText.textContent = score;
+
   createFood();
+
   drawFood();
+
   nextTick();
 }
+
 function nextTick() {
   if (running) {
     setTimeout(() => {
       clearBoard();
+
       drawFood();
+
       moveSnake();
+
       drawSnake();
+
       checkGameOver();
+
       nextTick();
     }, 75);
   } else {
     displayGameOver();
   }
 }
+
 function clearBoard() {
-  CSSTransition.fillStyle = boardBackground;
+  ctx.fillStyle = boardBackground;
+
   ctx.fillRect(0, 0, gameWidth, gameHeight);
 }
+
 function createFood() {
   function randomFood(min, max) {
     const randNum =
       Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+
     return randNum;
   }
-  foodX = randomFood(0, gameWidth, unitSize);
-  foodY = randomFood(0, gameWidth, unitSize);
+
+  foodX = randomFood(0, gameWidth - unitSize);
+
+  foodY = randomFood(0, gameWidth - unitSize);
 }
+
 function drawFood() {
   ctx.fillStyle = foodColor;
+
   ctx.fillRect(foodX, foodY, unitSize, unitSize);
 }
+
 function moveSnake() {
   const head = {
     x: snake[0].x + xVelocity,
@@ -97,14 +115,19 @@ function moveSnake() {
     snake.pop();
   }
 }
+
 function drawSnake() {
   ctx.fillStyle = snakeColor;
+
   ctx.strokeStyle = snakeBorder;
+
   snake.forEach((snakePart) => {
     ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
+
     ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
   });
 }
+
 function changeDirection(event) {
   const keyPressed = event.keyCode;
 
@@ -154,6 +177,67 @@ function changeDirection(event) {
       break;
   }
 }
-function checkGameOver() {}
-function displayGameOver() {}
-function resetGame() {}
+
+function checkGameOver() {
+  switch (true) {
+    case snake[0].x < 0:
+      running = false;
+
+      break;
+
+    case snake[0].x >= gameWidth:
+      running = false;
+
+      break;
+
+    case snake[0].y < 0:
+      running = false;
+
+      break;
+
+    case snake[0].y >= gameHeight:
+      running = false;
+
+      break;
+  }
+
+  for (let i = 1; i < snake.length; i += 1) {
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+      running = false;
+    }
+  }
+}
+
+function displayGameOver() {
+  ctx.font = "50px MV Boli";
+
+  ctx.fillStyle = "black";
+
+  ctx.textAlign = "center";
+
+  ctx.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2);
+
+  running = false;
+}
+
+function resetGame() {
+  score = 0;
+
+  xVelocity = unitSize;
+
+  yVelocity = 0;
+
+  snake = [
+    { x: unitSize * 4, y: 0 },
+
+    { x: unitSize * 3, y: 0 },
+
+    { x: unitSize * 2, y: 0 },
+
+    { x: unitSize, y: 0 },
+
+    { x: 0, y: 0 },
+  ];
+
+  gameStart();
+}
