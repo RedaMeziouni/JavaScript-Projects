9883ba2c83d25be726b6f36e448d0c50f8def667
RedaMeziouni <meziounidb@gmail.com>
2023-02-07 19:31:20 +0100

    Getting the Image Data from the HTML canvas

--- torch_effect/app.js
+++ torch_effect/app.js
@@ -1,4 +1,7 @@
 let CANVAS, CTX, VIDEO;
+
+const COLOR = [13, 25, 93];
+
 function main() {
   CANVAS = document.getElementById("myCanvas");
   CTX = CANVAS.getContext("2d");
@@ -20,5 +23,16 @@ function animateTorchEffect() {
   CANVAS.width = VIDEO.videoWidth;
   CANVAS.height = VIDEO.videoHeight;
   CTX.drawImage(VIDEO, 0, 0, CANVAS.width, CANVAS.height);
-  requestAnimationFrame(animateTorchEffect);
+
+  const { data } = CTX.getImageData(0, 0, CANVAS.width, CANVAS.height);
+  //   console.log(`rgb(${data[0]}, ${data[1]}), ${data[2]}`); //To get the blue color
+  for (let i = 0; i < data.length; i += 4) {
+    const r = data[i]; //red
+    const g = data[i + 1]; //green
+    const b = data[i + 2]; //blue
+    // const a = data[i + 3]; //alpha
+
+  }
+
+  //requestAnimationFrame(animateTorchEffect);
 }
